9bd6035591bb21900aa028cb0674b8acd1ceeb22
unknown <madnamv@gmail.com>
2020-11-27 16:31:43 -0400

    first commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,61 @@
+var socket = io();
+
+let params = new URLSearchParams(window.location.search)
+
+if (!params.has("nombre") || !params.has('sala')) {
+    window.location = "index.html"
+    throw new Error("El nombre y la sala son necesarios")
+}
+
+let usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit("entrarChat", usuario, (resp) => {
+
+        console.log("Usuarios conectados ", resp);
+
+    })
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('enviarMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor: ', mensaje);
+
+});
+
+//Escuchar cambios de usuarios
+//Cuando un usuario entra o sale del chat
+socket.on("listaPersona", (personas) => {
+
+    console.log(personas)
+
+})
+
+//Mensajes privados
+socket.on("mensajePrivado", (mensaje) => {
+
+    console.log("Mensaje privado", mensaje);
+
+})
